edc44385b8a4abf42decb713ca32afe975348e91
edm28contact-wq <edm28.contact@gmail.com>
2026-08-23 01:13:26 +0200

    feat: rafraîchir l'application après synchronisation Charlemagne

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CharlemagneMode from "./CharlemagneMode";
 import "./styles.css";
 
+function Root() {
+  const [appRevision, setAppRevision] = useState(0);
+
+  useEffect(() => {
+    const refresh = () => setAppRevision((revision) => revision + 1);
+    window.addEventListener("charlemagne-sync-updated", refresh);
+    return () => window.removeEventListener("charlemagne-sync-updated", refresh);
+  }, []);
+
+  return (
+    <>
+      <App key={appRevision} />
+      <CharlemagneMode />
+    </>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <CharlemagneMode />
-  </React.StrictMode>
+    <Root />
+  </React.StrictMode>,
 );
