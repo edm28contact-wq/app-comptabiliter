253f72f41356b230fedd4dda27957fe42c098644
edm28contact-wq <edm28.contact@gmail.com>
2026-08-22 02:19:06 +0200

    test: compiler les validateurs fournisseur

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod bank;
+mod identifiers;
 
 fn main() {
     let _ = bank::check_statement;
+    let _ = identifiers::is_valid_siret;
+    let _ = identifiers::is_valid_iban;
     app_comptabiliter_lib::run()
 }
